223a565ca1c11e989d23f8ed5d4d1e5fa84d99c0
jim-at-jibba <jim@justjibba.net>
2017-05-11 21:49:13 +0100

    Initialised a new elixir project

---
+++ lib/webscraper.ex
@@ -0,0 +1,18 @@
+defmodule Webscraper do
+  @moduledoc """
+  Documentation for Webscraper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Webscraper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/webscraper_test.exs
@@ -0,0 +1,8 @@
+defmodule WebscraperTest do
+  use ExUnit.Case
+  doctest Webscraper
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
