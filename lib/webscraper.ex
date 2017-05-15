8264569db5c09174703bd8d9c10aa60686cecaeb
jim-at-jibba <jim@justjibba.net>
2017-05-15 13:11:18 +0100

    Started setting out module structure and added get ingredients function

--- lib/webscraper.ex
+++ lib/webscraper.ex
@@ -4,15 +4,37 @@ defmodule Webscraper do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Webscraper.hello
-      :world
-
+    Main function that handles the processing of the response
   """
-  def hello do
-    :world
+  def main(url) do
+     case HTTPoison.get(url) do
+      {:ok, response} ->
+        case response.status_code do
+          200 -> 
+              response.body
+              |> get_ingredients
+              # |> Floki.find(".ingred-list")
+              # |> Enum.map(&strip_white_space)
+            {:ok}
+          -> :error
+        end
+    end
+  end
+
+  @doc """
+    Fetch ingredients from web scaper response
+  """
+  def get_ingredients(res) do
+    ingredients = res
+      |> Floki.find(".ingred-list")
+      |> Enum.map(&strip_white_space/1)
+      #%Webscraper.Recipe{ingredients: ingredients}
+
+      # return response from scraper
+      #res
+  end
+
+  def strip_white_space(item) do
+    item
   end
 end
